8d85a3c07bb35f8b27c057a4140af1344418afc1
chajus1 <chajus1@googlemail.com>
2023-01-17 23:05:58 +0000

    filling program class

--- HiLo/Program.cs
+++ HiLo/Program.cs
@@ -10,6 +10,20 @@ namespace HiLo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to HiLo.");
+            Console.WriteLine($"Guess umber between 1 and {HiLoGame.GetPot()}");
+            HiLo.Hint();
+            while (HiLo.Getpot() > )
+            {
+                Console.WriteLine("Press h for higher, l for lower, ? to buy a hint");
+                Console.WriteLine($"or any other key to quit with {HiLoGame.GetPot()}.");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == 'h') HiLoGame.Guess(true);
+                else if (key == 'l') HiloGame.Guess(false);
+                else if (key == '?') HiloGame.Hint();
+                else return;
+            }
+            Console.WriteLine("The pot is empty. Bye!");
         }
     }
 }
