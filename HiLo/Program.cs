21debd1d6b69c341e4995424ee079b03ed4eb605
chajus1 <chajus1@googlemail.com>
2023-01-18 03:14:06 +0000

    Seeded randomizer cheat

--- HiLo/Program.cs
+++ HiLo/Program.cs
@@ -13,9 +13,16 @@ namespace HiLo
             Console.WriteLine("Welcome to HiLo.");
             Console.WriteLine($"Guess number between 1 and {HiLoGame.MAXIMUM}.");
             HiLoGame.Hint();
+
+            HiLoGame.random = new Random(1);
+            Random seededRandom = new Random(1);
+            Console.Write("The first 20 numbers will be: ");
+            for (int i = 0; i < 10; i++)
+                Console.Write($"{seededRandom.Next(1, HiLoGame.MAXIMUM + 1)}, ");
+
             while (HiLoGame.GetPot() > 0)
             {
-                Console.WriteLine("Press h for higher, l for lower, ? to buy a hint");
+                Console.WriteLine("\nPress h for higher, l for lower, ? to buy a hint");
                 Console.WriteLine($"or any other key to quit with {HiLoGame.GetPot()}.");
                 char key = Console.ReadKey(true).KeyChar;
                 if (key == 'h') HiLoGame.Guess(true);
@@ -26,6 +33,8 @@ namespace HiLo
             }
             Console.WriteLine("The pot is empty. Bye!");
             Console.WriteLine("Press 0  to cheat");
+
+            
         }
     }
 }
